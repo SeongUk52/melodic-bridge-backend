826bd2c58dd329fa95f59843f82d586b52d37474
SeongUk52 <rkfka7849@gmail.com>
2024-11-17 19:08:29 +0900

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/melodicbridge/melodic_bridge/MelodicBridgeApplication.java
@@ -0,0 +1,13 @@
+package com.melodicbridge.melodic_bridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelodicBridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MelodicBridgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/melodicbridge/melodic_bridge/MelodicBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melodicbridge.melodic_bridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MelodicBridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
