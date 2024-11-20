5b1eb7ca2be4b138f8d1290bd8c0d8fd600224dc
SeongUk52 <rkfka7849@gmail.com>
2024-11-20 23:43:50 +0900

    feat: set Config

---
+++ src/main/java/com/melodicbridge/melodic_bridge/config/AppConfig.java
@@ -0,0 +1,9 @@
+package com.melodicbridge.melodic_bridge.config;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@AutoConfiguration
+public class AppConfig {
+}
